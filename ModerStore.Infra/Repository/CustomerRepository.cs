3f179030e5c657442e599e014ca26985b6f92c57
RaMadaSilva <raul01mateia@gmail.com>
2023-05-12 09:51:38 +0100

    actualização minima

--- ModerStore.Infra/Repository/CustomerRepository.cs
+++ ModerStore.Infra/Repository/CustomerRepository.cs
@@ -11,48 +11,48 @@ namespace ModernStore.Infra.Repository
     {
         private readonly ModernStoreDbContext _context;
 
-        public CustomerRepository(ModernStoreDbContext context) 
+        public CustomerRepository(ModernStoreDbContext context)
             => _context = context;
-        
+
         public bool DocumentExists(string document)
-            => _context.Customers.Any(x=>x.Document.Number ==document);
+            => _context.Customers.Any(x => x.Document.Number == document);
 
         public Customer Get(Guid id)
         {
             var customer = _context.Customers
-                .Include(x=>x.User)
+                .Include(x => x.User)
                 .AsNoTracking()
                 .Where(x => x.User.Id == id)
                 .FirstOrDefault();
 
-            return customer; 
+            return customer;
         }
 
         public ICommandResult Get(string userName)
         {
             var result = _context
-               .Customers
-               .Include(x => x.User)
-               .AsNoTracking()
-               .Where(x=>x.User.UserName==userName)
-               .Select(x => new GetCustomerCommandResult(
+                .Customers
+                .Include(x => x.User)
+                .AsNoTracking()
+                .Where(x => x.User.UserName == userName)
+                .Select(x => new GetCustomerCommandResult(
                     x.Name.ToString(),
                     x.Document.Number,
                     x.Email.Adress,
                     x.User.UserName,
                     x.User.Passeword,
                     x.User.Active))
-               .FirstOrDefault();
+                .FirstOrDefault();
 
-            return result; 
+            return result;
         }
 
-        public void Save(Customer customer) 
+        public void Save(Customer customer)
             => _context.Customers.Add(customer);
-        
 
-        public void Update(Customer customer) 
-            => _context.Entry(customer).State = EntityState.Modified; 
-        
+
+        public void Update(Customer customer)
+            => _context.Entry(customer).State = EntityState.Modified;
+
     }
 }
